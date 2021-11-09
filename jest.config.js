3301ee77f505f31c23e7a6e613c1d283b3db3b88
Hasan Sever <pal_sever@live.com>
2021-11-10 02:46:35 +0300

    add jest reporters

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@ module.exports = {
   // setupFilesAfterEnv: ['./tests/setup.js'],
   testTimeout: 5000,
   verbose: true,
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputName: 'test-results.xml',
+      },
+    ],
+  ],
 };
